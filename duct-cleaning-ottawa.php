8f38c0e75d3680296cbcdc1c818a1d93f643d071
Haghgooie <Amir.Haghgooie@ec.gc.ca>
2016-12-29 15:18:44 -0500

    Converted htmls to php

---
+++ duct-cleaning-ottawa.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html amp lang="en-CA">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<script async src="https://cdn.ampproject.org/v0.js"></script>
+	
+	<meta content="Duct Cleaning with Powerful Truck-Mounted Equipment. We are ready to help. Call us at 613-800-9022 or visit our web site." name="description" />
+	<title>Zenith Eco Energy - Air Duct Cleaning in Ottawa Region | </title>
+	<meta content="index, follow" name="robots" />
+	<meta content="Air Duct Cleaning Ottawa, Duct Cleaning Ottawa, Ottawa Duct Cleaning" name="keywords" />
+	<link href="https://plus.google.com/112761760079230211083" rel="publisher" />
+	<meta content="en-CA" property="og:locale" />
+	<meta content="article" property="og:type" />
+	<meta content="Duct Cleaning in Ottawa, Ontario | Zenith Eco Energy" property="og:title" />
+	<meta content="Duct Cleaning with Powerful Truck-Mounted Equipment. We are ready to help. Call us at 613-800-9022 or visit our web site." property="og:description" />
+	<meta content="http://zenitheco.ca/" property="og:url" />
+	<meta content="Zenith Eco Energy Heating and Cooling" property="og:site_name" />
+	<meta content="https://www.facebook.com/Zenith-Eco-Energy-Inc-1410204525905168/" property="article:publisher" />
+	<meta content="http://zenitheco.ca/blog/wp-content/uploads/2016/01/zenithL500x500.png" property="og:image" />
+	<meta content="summary" name="twitter:card" />
+	<meta content="Duct Cleaning with Powerful Truck-Mounted Equipment. We are ready to help. Call us at 613-800-9022 or visit our web site." name="twitter:description" />
+	<meta content="Duct Cleaning in Ottawa, Ontario | Zenith Eco Energy Inc." name="twitter:title" />
+	<meta content="@ZenithEco" name="twitter:site" />
+	<meta content="http://zenitheco.ca/blog/wp-content/uploads/2016/01/zenithL500x500.png" name="twitter:image" />
+	<meta content="@ZenithEco" name="twitter:creator" />
+	<link href="http://gmpg.org/xfn/11" rel="profile" />
+	<link href="http://zenitheco.ca/blog/xmlrpc.php" rel="pingback" />
+	<link href="images/favicon.ico" rel="icon" type="image/x-icon" />
+	<link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	<link href="http://zenitheco.ca/blog/wp-content/uploads/2015/07/cropped-zenith-seethrough-32x32.png" rel="icon" sizes="32x32" />
+	<link href="http://zenitheco.ca/blog/wp-content/uploads/2015/07/cropped-zenith-seethrough-192x192.png" rel="icon" sizes="192x192" />
+	<link href="http://zenitheco.ca/blog/wp-content/uploads/2015/07/cropped-zenith-seethrough-180x180.png" rel="apple-touch-icon-precomposed" />
+	<link href="css/style.css" media="screen" rel="stylesheet" type="text/css" />
+	<link href="css/camera.css" id="camera-css" media="all" rel="stylesheet" type="text/css" /><script defer type="text/javascript" src="js/jquery.js"></script><script defer type="text/javascript" src="js/superfish.js"></script><script defer type="text/javascript" src="js/jquery.responsivemenu.js"></script><script defer type="text/javascript" src="js/jquery.mobilemenu.js"></script><script defer src="js/jquery.ui.totop.js"></script><!--[if (gt IE 9)|!(IE)]&gt;<!--><!-- </script> --><!--<![endif]--><!-- <script>
+     	$(document).ready(function(){
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			jQuery('#camera_wrap').camera({
+				loader: false,
+				pagination: true,
+				thumbnails: false,
+				height: '52.52631578947368%',
+				caption: true,
+				navigation: false,
+				fx: 'simpleFade'
+			});
+		});
+		
+     </script> --><!--[if lt IE 8]>
+   <div style=' clear: both; text-align:center; position: relative;'>
+     <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+       <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+    </a>
+  </div>
+<![endif]--><!-- [if lt IE 9]>
+	<script src="js/html5.js"></script>
+	<link rel="stylesheet" href="css/ie.css">
+	<link href='http://fonts.googleapis.com/css?family=Lato:400' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:700' rel='stylesheet' type='text/css'>
+<![endif] --><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-70101205-1', 'auto');
+  ga('send', 'pageview');
+</script>
+	<meta content="width=300, initial-scale=1" name="viewport" />
+	<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+
+</head>
+<body class="page1" itemscope="" itemtype="http://schema.org/LocalBusiness"></body>
+<meta content="Zenith Eco Energy Inc." itemprop="name" />
+<meta content="http://zenitheco.ca" itemprop="url" />
+<div itemprop="ContactPoint" itemscope="" itemtype="http://schema.org/ContactPoint"></div>
+<meta content="+16138009022" itemprop="telephone" />
+<meta content="info@zenitheco.ca" itemprop="email" />
+<meta content="Customer Support" itemprop="contactType" />
+<!-- Header -->
+<?php include("../commonheader.php"); ?>
+<!--Content-->
+
+<div class="page1_bg">
+<div class="middle">
+<div class="container_16">
+<div class="wrapper">
+<div class="grid_10" style="width:90%"><a class="OnlineAppointment" href="/blog/make-appointment-online/" title="Book appointment online | zenitheco"><img src="/blog/wp-content/uploads/2016/03/OnlineAppointmentIcon-e1457416777344.png" /></a>
+
+<h1>Need Air Duct Cleaning in Ottawa?</h1>
+&nbsp;
+
+<p class="MsoNormal">If your ducts look dirty, you should definitely consider duct cleaning.<o:p></o:p></p>
+
+<p class="MsoNormal">Your&nbsp;<a href="http://zenitheco.ca/furnace-repair-ottawa.html">furnace</a>, ventilation, and&nbsp;<a href="http://zenitheco.ca/aircon.html">air conditioning</a> systems all use ducts to transfer air. The ducts can become filled with dust, mold, and allergens, especially if you have done any home improvement, you have just moved into your home, or you notice any mould or water in your ducts. Your HVAC system is responsible for air circulation, and a build-up of contaminants in your ducts means the system must work harder. This extra pressure can shorten the life of your system. These pollutants not only create an unhealthy home environment but also reduce your&nbsp;<a href="http://zenitheco.ca/blog/expert-hvac-advice/">HVAC</a>&nbsp;system&rsquo;s efficiency.<o:p></o:p></p>
+
+<p></p>
+
+<p class="MsoNormal">Your ducts should be cleaned at least once every three to five years to help maintain strong indoor air quality and optimal operation of your&nbsp;<a href="http://zenitheco.ca/furnace-repair-ottawa.html">heating</a>, &nbsp;ventilation, and&nbsp;<a href="http://zenitheco.ca/aircon.html">air conditioning systems</a>. At Zenith, we use a high-efficient particle air vacuuming equipment to clean your ducts, and always take care to protect your duct work.&nbsp;</p>
+
+<figure style=" display:inline; font-size:1em; margin: 10px 50px 10px 30px; line-height: 30px; "><img alt="Air Duct Cleaning in Ottawa, Ontario - Air Duct Before and After Cleaning" class="roundcorners" src="/images/airduct3.jpg" /> <figcaption style=" padding:10px 0px 20px 10px ">
+<p>&nbsp; &nbsp; &nbsp; &nbsp;Air Duct - before and after cleaning.</p>
+
+<p></p>
+
+<div style="background:#eee;border:1px solid #ccc;padding:5px 10px;"><a href="#" onclick="$('#hiddenDC').show(); return false;">See what&#39;s included</a></div>
+
+<div id="hiddenDC" style="display: none;">
+
+
+<ol>
+<p>Return Duct</p>
+	<li>Furnace Operation Inspection</li>
+	<li>Condensing System Inspection</li>
+	<li>A-Coil Inspection</li>
+	<li>Cold Air Returns From Furnace Isolation</li>
+	<li>Installation of Vacuum to Main Return Duct</li>
+	<li>Camera Inspection</li>
+	<li>Air-whip and Cold Air Sweep</li>
+	<li>Return Main Duct Cleaning</li>
+	<li>Inspection for Proper Cleanliness</li>
+	<li>Re-Clean (if necessary)</li>
+	<li>Sanitizer Application</li>
+	<li>Re-installation of all registers</li>
+
+<p>Supply Duct</p>
+
+	<li>Removal and Sealing of All Vents</li>
+	<li>Camera Inspection</li>
+	<li>Installation of Vacuum to the Main Supply Duct</li>
+	<li>Air-Whip and Sweeping of All Supply Vents</li>
+	<li>Inspection For Proper Cleaning</li>
+	<li>Re-Clean (if necessary)</li>
+	<li>Sanitizer Application</li>
+	<li>Installation of One-Inch Plugs</li>
+	<li>Floor and Wall Registers Cleaning</li>
+	<li>Furnace Cleaning</li>
+	<li>Furnace Check and Re-start</li>
+	<li>Certificate Installation</li>
+	<li>Advise customer of any problems</li>
+</ol>
+</div>
+
+<p></p>
+
+<p><a href="http://zenitheco.ca/blog/make-appointment-online/">Book online</a> or call us at 613-800-9022 and schedule an appointment. Your heating and cooling system will thank you for it. Don&#39;t forget to check<a href="https://www.facebook.com/Zenith-Eco-Energy-Inc-1410204525905168/"> our Facebook page for promotions</a>!</p>
+</figcaption></figure>
+</div>
+</div>
+</div>
+</div>
+
+<div class="clear"></div>
+</div>
+<!--<div class="page1_bot">
+<div class="top">
+<div class="container_16">
+<div class="grid_4">
+<h2><a href="http://zenitheco.ca/promo.html"><span style="font-size:18px;">Heating &amp; Cooling Financing</span></a></h2>
+<div class="img_inner"><img alt="Heating and Cooling Financing in Ottawa" src="images/page1_img2.jpg" /></div>
+<p>We can help you with your heating and cooling system purchase with financing specialists. Simply contact us and we will take care of the rest.</p>
+<a class="btn" href="promo.html">learn more</a></div>
+<div class="grid_4">
+<h2><a href="http://zenitheco.ca/heating.html"><span style="font-size:18px;">AC , Furnace Repair &amp; Services</span></a></h2>
+<div class="img_inner"><img alt="AC and Furnace Repair and Service in Ottawa | Zenith Eco Inc." src="images/page1_img3.jpg" /></div>
+<p>Having AC and furnace issues? You don&#39;t need to worry. We provide quick and reliable service for&nbsp;all makes and models!</p>
+<a class="btn" href="heating.html">learn more</a></div>
+<div class="grid_4">
+<h2><a href="http://zenitheco.ca/heating.html"><span style="font-size:18px;">Maintenance &amp; Tune-ups</span></a></h2>
+<div class="img_inner"><img alt="Services for heating, cooling, water heaters, tank-less heaters, and many other home appliances in Ottawa | | Zenith Eco Inc." src="images/page1_img4.jpg" /></div>
+<p>We offer services for heating, cooling, water heaters, tankless heaters, and many other home appliances.</p>
+<a class="btn" href="heating.html">learn more</a></div>
+<div class="clear"></div>
+</div>
+</div>
+-->
+
+<div class="bot">
+<div class="container_16">
+<div class="grid_4"><strong><span style="color:white;">site map</span></strong>
+
+<ul class="list_1">
+	<li><a href="http://zenitheco.ca/index.html">Home</a></li>
+	<li><a href="http://zenitheco.ca/furnace-repair-ottawa.html">Furnace Repair</a></li>
+	<li>P<a href="http://zenitheco.ca/promotions.html">romotions</a></li>
+	<li><a href="http://zenitheco.ca/aboutus.html">About us</a></li>
+	<li><a href="http://zenitheco.ca/contactus.html">Contact us</a></li>
+	<li><a href="http://zenitheco.ca/blog/">Expert&#39;s blog</a></li>
+</ul>
+</div>
+<!--	<div class="grid_4">
+	                	<h4>elsewhere</h4>
+	                    <div class="social">
+	                    	<div>
+		                    	
+		                    	<a href="#"><img src="images/rss_icon.png" alt="RSS Feed"> Rss feed</a>
+	                            <div class="clear"></div>
+	                        </div>
+	                        <div>
+		                    	
+		                    	<a href="#"><img src="images/fb_icon.png" alt="ZenithFacebook"> Facebook</a>
+	                            <div class="clear"></div>
+	                        <div>
+	                        </div>
+		                    	
+		                    	<a href="#"> <img src="images/twitter_icon.png" alt="">Twitter</a>
+	                            <div class="clear"></div>
+	                        </div>
+	                    </div> 
+	                </div>-->
+
+<div class="grid_4"><strong><span style="color:white;">Contact Info</span></strong> <!-- Go to www.addthis.com/dashboard to customize your tools --><!-- <div class="addthis_horizontal_follow_toolbox"></div> -->
+
+<div class="phone1"><span>+1 613 800 9022</span> <a href="mailto:info@zenitheco.ca">info@zenitheco.ca</a></div>
+
+<address>13 - 2100 Thurston Drive Ottawa, Ontario<br />
+K1G 4K8</address>
+&nbsp;<br />
+<a href="http://www.bbb.org/ottawa/business-reviews/air-conditioning-and-heating-contractors-residential/zenith-ecoenergy-in-ottawa-on-55260" rel="nofollow" target="_blank" title="Click for the Business Review of Zenith Eco Energy, a Heating, Air Conditioner and Air Duct Cleaning Contractors in Ottawa, Ontario"><img alt="Click for the BBB Business Review of this Heating, Air Conditioner and Air Duct Cleaning Contractors in Ottawa, Ontario" src="/blog/wp-content/uploads/2016/03/cbbb-badge-horz120x45.png" style="border: 0;" /></a> &nbsp; <strong><u><a href="http://zenithEco.ca/privacy-policy.html"><span style="color:#FFFFFF;">Privacy Notice</span></a></u></strong></div>
+
+<div class="clear"></div>
+</div>
+</div>
+<!-- Footer end--><!-- Go to www.addthis.com/dashboard to customize your tools --><!-- <script async type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5591aea00ffea219"></script> --><!-- Go to www.addthis.com/dashboard to customize your tools --><!--<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5591aea00ffea219" async></script> --><script type="text/javascript">
+function downloadJSAtOnload() {
+var element = document.createElement("script");
+element.src = "js/jquery.responsivemenu.js";
+document.body.appendChild(element);
+var element = document.createElement("script");;
+element.src = "js/jquery.mobilemenu.js";
+document.body.appendChild(element);
+var element = document.createElement("script");
+element.src = "js/jquery.easing.1.3.js";
+document.body.appendChild(element);
+var element = document.createElement("script");
+element.src = "js/jquery.ui.totop.js";
+document.body.appendChild(element);
+var element = document.createElement("script");
+element.src = "js/camera.js";
+document.body.appendChild(element);
+var element = document.createElement("script");
+element.src = "js/jquery.mobile.customized.min.js";
+document.body.appendChild(element);
+}
+if (window.addEventListener)
+window.addEventListener("load", downloadJSAtOnload, false);ū
+else if (window.attachEvent)
+window.attachEvent("onload", downloadJSAtOnload);
+else window.onload = downloadJSAtOnload;
+</script><!-- Footer end--></html>
